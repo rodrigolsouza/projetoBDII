109bcddbc25e21e95c6579ca2789812776f85dbe
rodrigolsouza <rodrigo.lopessouza@hotmail.com>
2022-07-14 20:27:39 -0300

    Revert "atualização 14-07-22 (implementação complexa)"
    This reverts commit 704917418dff09f3d56a0943d3d6ebc254c6df5a.

--- index.py
+++ index.py
@@ -1,11 +1,28 @@
-
-from conexao import *
+import pymongo
 from utilitarios import *
+from Linha import *
+from Empresa import *
 
-#Conexão com o banco de dados
-conexao.conectar()
+
+#Declaração das constantes necessárias na conexão
+BASE_DE_DADOS="Gestao_transportes"
+COLECAO_1="empresas"
+STRING_DE_CONEXAO="mongodb+srv://admin:admin@cluster0.ynpyc9f.mongodb.net/?retryWrites=true&w=majority"
+
+try:
+    #Realização da conexão com o servidor na nuvem
+    cliente = pymongo.MongoClient(STRING_DE_CONEXAO)
+    bancoDeDados=cliente[BASE_DE_DADOS]
+    colecao1=bancoDeDados[COLECAO_1]
+
+    #Teste para comprovar que o cluster está funcionando
+    print("list_database_names: ", cliente.list_database_names())
+    
+#Mensagem de erro caso a conexão falhe
+except pymongo.errors.ConnectionFailure as errorConexion:
+    print("Falha ao conectar-se ao MongoDB " +errorConexion)
 
 #Exibição do Menu de atividades operacionais
-showMenu.menu_inicial()
-showMenu.exibir_menu_inicial()
+menu_inicial()
+exibir_menu_inicial()
 
